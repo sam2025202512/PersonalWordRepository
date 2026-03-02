79c1c7386ee5c65af386d9c5e1bdf96e155fa9c1
saaralaasone <71126251+saaralaasone@users.noreply.github.com>
2026-03-02 14:36:07 +0200

    Update api.py

--- wordrepo/api.py
+++ wordrepo/api.py
@@ -11,4 +11,22 @@ def create_app():
 
   db.init_app(app)
   api = Api(app)
+
+  #import resources
+  from wordrepo.resources.user import UserListResource, UserResource
+  from wordrepo.resources.word import WordListResource, WordResource
+  from wordrepo.resources.translation import TranslationListResource, TranslationResource
+  from wordrepo.resources.category import CategoryListResource, CategoryResource
+  from wordrepo.resources.part_of_speech import PartOfSpeechListResource
+  from wordrepo.resources.quiz import QuizListResource, QuizResource
+
+  #User endpoints
+  api.add_resource(UserListResource, "/users")
+  api.add_resource(UserResource, "/users/<string:user_id>")
+  #Word endpoints
+  api.add_resource(WordListResource, "/words")
+  api.add_resource(WordResource, "/words/<string:word_id>")
+  #Translation endpoints
+  api.add_resource(TranslationListResource, "/words/<string:word_id>/translations")
+  api.app_resource(TranslationResource, "/translations/<string:translation_id>")
   return app
