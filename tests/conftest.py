d971e772a2b6cf3087592a0170cfa0ebf3c6ad78
saaralaasone <71126251+saaralaasone@users.noreply.github.com>
2026-03-02 16:51:17 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,3 +10,12 @@ def app():
     db.create_all()
     yield app
     db.drop_all()
+
+@pytest.fixture
+def client(app):
+  return app.test_client()
+
+@pytest.fixture
+def db_session(app):
+  with app.app_context():
+    yield db.session
